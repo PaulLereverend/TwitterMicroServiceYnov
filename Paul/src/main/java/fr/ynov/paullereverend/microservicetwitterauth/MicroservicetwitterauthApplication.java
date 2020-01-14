48a7f56edf29e41844ab281191ed7e3f301ebc67
Paul <paulereverend@gmail.com>
2020-01-14 09:06:37 +0100

    Init spring project

---
+++ Paul/src/main/java/fr/ynov/paullereverend/microservicetwitterauth/MicroservicetwitterauthApplication.java
@@ -0,0 +1,13 @@
+package fr.ynov.paullereverend.microservicetwitterauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicetwitterauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicetwitterauthApplication.class, args);
+	}
+
+}
---
+++ Paul/src/test/java/fr/ynov/paullereverend/microservicetwitterauth/MicroservicetwitterauthApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ynov.paullereverend.microservicetwitterauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicetwitterauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

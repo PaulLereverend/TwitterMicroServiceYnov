d85f3cec4c6a2b25f46697b6b7fd3338963215f1
loicbouchain <loic.bouchain@orange.fr>
2020-01-20 13:43:47 +0100

    ajout bdd

---
+++ b01/twitterapi/src/main/java/com/twitterapi/twitterapi/User.java
@@ -0,0 +1,42 @@
+package com.twitterapi.twitterapi;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String username;
+
+  private String password;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return username;
+  }
+
+  public void setName(String name) {
+    this.username = name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
+
---
+++ b01/twitterapi/src/main/java/com/twitterapi/twitterapi/UserRepository.java
@@ -0,0 +1,10 @@
+package com.twitterapi.twitterapi;
+
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	User findByUsernameAndPassword(String username, String password);
+}

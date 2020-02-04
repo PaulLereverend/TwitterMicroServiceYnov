9748419c90f8e9185ccc5a928f78d585ef90f299
Moi <valentin.dezat@gmail.com>
2020-02-04 11:47:34 +0100

    add createTweet

---
+++ b04/src/main/java/com/ecriretweet/ecriretweet/tweet.java
@@ -0,0 +1,27 @@
+package com.ecriretweet.ecriretweet;
+
+public class tweet{
+	private String id;
+	private String letweet;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTweet() {
+		return letweet;
+	}
+
+	public void setTweet(String tweet) {
+		this.letweet = tweet;
+	}
+	
+	
+	
+	
+	
+}
---
+++ b04/src/test/java/com/ecriretweet/ecriretweet/EcriretweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecriretweet.ecriretweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcriretweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
